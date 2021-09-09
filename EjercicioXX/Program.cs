aaec117dc54589781d59df79b74c2bbb89896aa2
EmmanuelLeguizamon <ema.legui.carp@gmail.com>
2021-09-09 19:32:39 -0300

    Ejercicio 20

--- EjercicioXX/Program.cs
+++ EjercicioXX/Program.cs
@@ -5,8 +5,58 @@ namespace EjercicioXX
     class Program
     {
         static void Main(string[] args)
+
+
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo cki;
+
+            do
+            {
+                bool isString;
+                string n1, n2;
+                Console.WriteLine("'Vamos a determinar cual es el mayor entre 2 números'");
+
+
+                Console.Write("Ingrese el primer número: \n");
+                n1 = Console.ReadLine();
+
+                isString = Decimal.TryParse(n1, out decimal m1);
+
+
+
+                Console.Write("Ingrese el segundo número: \n");
+                n2 = Console.ReadLine();
+
+
+                Decimal.TryParse(n2, out decimal m2);
+
+                if (m1 > m2)
+                {
+                    Console.WriteLine("El primer numero es mayor al segundo");
+                }
+                else
+                {
+                    if (m2 > m1)
+                    {
+                        Console.WriteLine("El segundo numero es mayor al primero");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Los dos numeros son iguales");
+                    }
+                }
+               
+               
+                Console.WriteLine("Si queres salir, apreta 'S'. Sino, apreta cualquier tecla.");
+                cki = Console.ReadKey();
+
+
+                Console.Clear();
+            } while (cki.Key != ConsoleKey.S);
+
+            
+          
         }
     }
 }
